5f25a3084ce54a2cdf945c9e93d131842f9bcc0a
sjc5182 <shiguangchen1987@gmail.com>
2019-01-30 02:27:06 -0800

    rout query

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
 const express = require('express');
 const path = require('path');
 const { Pool, Client } = require('pg');
-
-const sequelize = require('./app/config/db.config');
-
-sequelize.authenticate().then(() => console.log('Database connect...')).catch(err => console.log('Error' + err))
+const cors = require('cors');
 
 
+const config = {
+  database: 'booking_db',
+  username: 'postgres',
+  password: 'kalin1987',
+  host: 'localhost',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+}
+//const sequelize = require('./app/config/db.config');
 
-const pool = new Pool(sequelize);
+//sequelize.authenticate().then(() => console.log('Database connect...')).catch(err => console.log('Error' + err))
 
-// pool.query('SELECT * from FoodIngd', (err, res) => {
-//   console.log(err, res)
-//   pool.end()
-// })
 
-pool.connect((err, client, done) => {
-  if (err) throw err
-  client.query('SELECT * FROM public."FoodIngd" WHERE id = 1', (err, res) => {
-    done()
-    if (err) {
-      console.log(err.stack)
-    } else {
-      console.log(res.rows[0])
-    }
-  })
-})
+
+const pool = new Pool(config);
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Index'));
+app.use(cors());
+
+app.get('/food', (request, response) => {
+  pool.connect((err, db, done) => {
+    if (err) {
+      return response.status(400).send(err)
+    }
+    else {
+      db.query('SELECT * FROM public."FoodIngd"', (err, table) => {
+        done();
+        if(err) {
+          return response.status(400).send(err)
+        }
+        else {
+          return response.status(200).send(table.rows)
+        }
+      })
+    }
+  })
+});
 
 const PORT = process.env.PORT || 8000;
 
